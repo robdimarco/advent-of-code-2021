2554904e73bb7c80edbb4a3d887052f8c4354fa6
Rob Di Marco <rob@innovationontherun.com>
2021-12-07 07:35:19 -0500

    Part 2

--- day7.rb
+++ day7.rb
@@ -1,10 +1,29 @@
 data = File.read('day7.txt').split(',').map(&:to_i)
 # data = "16,1,2,0,4,2,7,1,2,14".split(',').map(&:to_i)
+@move_costs = {}
 
-def fuel_required(data, pos)
-  data.map {|p| (p - pos).abs}.sum
+def move_cost(i)
+  return 0 if i <= 0
+
+  @move_costs[i] ||= move_cost(i - 1) + i
 end
 
+def fuel_required_1(data, pos)
+  data.map {|p| (p - pos).abs}.sum
+end
+def fuel_required_2(data, pos)
+  data.map {|p| move_cost((p - pos).abs)}.sum
+end
 
-min_pos = (0..data.max).min_by {|pos| fuel_required(data, pos)}
-puts [min_pos, fuel_required(data, min_pos)].inspect
+def part_1(data)
+  min_pos = (0..data.max).min_by {|pos| fuel_required_1(data, pos)}
+  puts [min_pos, fuel_required_1(data, min_pos)].inspect
+end
+
+def part_2(data)
+  min_pos = (0..data.max).min_by {|pos| fuel_required_2(data, pos)}
+  puts [min_pos, fuel_required_2(data, min_pos)].inspect
+end
+
+# part_1(data)
+part_2(data)
