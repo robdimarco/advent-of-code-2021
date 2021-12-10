33c67de272eccdcfe0ec696d1246d94b67dcbe12
Rob Di Marco <rob@innovationontherun.com>
2021-12-10 01:00:01 -0500

    Day 10 part 2

--- day10.rb
+++ day10.rb
@@ -26,6 +26,13 @@ scores = {
   '>' => 25137,
 }
 
+close_scores = {
+  ')' => 1,
+  ']' => 2,
+  '}' => 3,
+  '>' => 4,
+}
+
 def validate_line(line, pairs)
   stack = []
   line.chars.each do |c|
@@ -41,4 +48,29 @@ def validate_line(line, pairs)
   nil
 end
 
-puts lines.map {|l| validate_line(l, pairs)}.compact.map {|c| scores[c]}.sum
+puts lines.map {|l| validate_line(l, pairs)}.compact.map {|l| scores[l]}.sum
+incomplete = lines.reject {|l| validate_line(l, pairs)}
+
+vals = incomplete.map do |line|
+  stack = []
+  line.chars.each do |c|
+    if pairs.key?(c)
+      # puts "push #{c}"
+      stack.push c
+    else
+      top = stack.pop
+      # puts "compare c to #{pairs[top]}"
+      break unless pairs[top] == c
+    end
+  end
+
+  total_score = 0
+  while stack.size > 0 do
+    c = stack.pop
+    total_score *= 5
+    total_score += close_scores[pairs[c]]
+  end
+  total_score
+end
+
+puts vals.sort[vals.size/2]
