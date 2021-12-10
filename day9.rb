8afc8303901ab7b25d21256585f944428670c281
Rob Di Marco <rob@innovationontherun.com>
2021-12-09 20:24:38 -0500

    Finish part 2

--- day9.rb
+++ day9.rb
@@ -34,5 +34,47 @@ def find_low_points(grid)
   end
   all_low_points
 end
-puts find_low_points(grid).reduce(0) {|acc, p| acc + p[2] + 1}
+
+def neighboring_points_in_basin(grid, x, y)
+  # puts "checking [#{x}, #{y}]"
+  rv = []
+  [[1, 0], [-1, 0], [0, 1], [0, -1]].each do |(dx, dy)|
+    x1 = dx +x
+    y1 = dy +y
+    next if x1 < 0 || y1 < 0 || x1 >= grid[0].size || y1 >= grid.size || grid[y1][x1] == 9
+    rv << [x1, y1]
+  end
+  rv
+end
+
+def find_basins(grid)
+  lps = find_low_points(grid)
+  basins = []
+
+  lps.each do |low_point|
+    basin = [[low_point[0], low_point[1]]]
+    points_to_check = [low_point]
+    checked_points = []
+    while points_to_check.any? do
+      point = points_to_check.shift
+      next if checked_points.include?(point)
+      checked_points << point
+      # puts "points_to_check #{points_to_check}"
+      # puts "checked_points #{checked_points}"
+      nps = neighboring_points_in_basin(grid, point[0], point[1])
+      # puts "np #{nps}"
+      nps.each do |np|
+        if !basin.include?(np)
+          basin << np
+          points_to_check << np unless points_to_check.include?(np)
+        end
+      end
+    end
+    # puts basin.inspect
+    basins << basin
+  end
+  basins
+end
+# puts find_low_points(grid).reduce(0) {|acc, p| acc + p[2] + 1}
+puts find_basins(grid).map(&:size).sort.reverse[0..2].reduce(:*)
 
