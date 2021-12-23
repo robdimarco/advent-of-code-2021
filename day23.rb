f67f0453fecf4ae887fa43260ae6dcf6449f5b7a
Rob Di Marco <rob@innovationontherun.com>
2021-12-23 15:56:56 -0500

    Day 23 part 2

--- day23.rb
+++ day23.rb
@@ -14,7 +14,7 @@ World = Struct.new(:rooms, :hall) do
     return false if hall.compact.size > 0
     rooms.each_with_index do |room, idx|
       l = ROOMS[idx]
-      return false unless room == [l, l]
+      return false unless room.uniq == [l]
     end
 
     true
@@ -25,14 +25,14 @@ World = Struct.new(:rooms, :hall) do
   end
 
   def next_moves
+    # pp "Checking #{token}"
     return [] if done?
     rv = []
     hall.each_with_index do |l, i|
       next unless l
       eligible_room = l.hex - 'A'.hex
       next unless rooms[eligible_room][0].nil?
-      next unless rooms[eligible_room][1].nil? || rooms[eligible_room][1] == l
-
+      next unless [[], [l]].include?(rooms[eligible_room].uniq.compact)
 
       room_loc = (eligible_room + 1) *2
 
@@ -44,13 +44,14 @@ World = Struct.new(:rooms, :hall) do
       w.hall[i] = nil
       energy_used = (i-room_loc).abs * ENERGY_MAP[l]
 
-      if rooms[eligible_room][1].nil?
-        w.rooms[eligible_room][1] = l
-        energy_used += 2 * ENERGY_MAP[l]
-      else
-        w.rooms[eligible_room][0] = l
-        energy_used += ENERGY_MAP[l]
+      (0...rooms[eligible_room].size).each do |j|
+        if rooms[eligible_room][j].nil?
+          w.rooms[eligible_room][j] = l
+          w.rooms[eligible_room][j - 1] = nil if j > 0
+          energy_used += ENERGY_MAP[l]
+        end
       end
+
       rv.push([w, energy_used])
     end
 
@@ -59,19 +60,27 @@ World = Struct.new(:rooms, :hall) do
       energy_out = nil
       room_loc = nil
 
-      if room[0]
-        l = room[0]
-        energy_out = ENERGY_MAP[l]
-        room_loc = 0
-      elsif room[1]
-        l = room[1]
-        energy_out = 2 * ENERGY_MAP[l]
-        room_loc = 1
+      (0...room.size).each do |j|
+        if room[j]
+          l = room[j]
+          energy_out = (j+1) * ENERGY_MAP[l]
+          room_loc = j
+          break;
+        end
       end
 
       next unless l
-      next if i == l.hex - 'A'.hex && room_loc == 1 # Do not move if in right room
-      next if i == l.hex - 'A'.hex && room_loc == 0 && room[1] == l # Do not move if in right room
+      if i == l.hex - 'A'.hex
+        all_below_are_right_letter = true
+        # puts "checking letter for #{l} in room #{i}"
+        ((room_loc+1)...room.size).each do |j|
+          # puts "position #{j} is #{room[j]} in room #{i} for #{token}"
+          all_below_are_right_letter &&= room[j] == l
+        end
+
+        next if all_below_are_right_letter
+      end
+
 
       valid_hall_spots = (0...hall.size).to_a.map do |h|
         next if hall[h]
@@ -85,12 +94,14 @@ World = Struct.new(:rooms, :hall) do
       end.compact
 
       valid_hall_spots.each do |h|
+        # pp "Checking from #{token}"
         w = deep_clone
         w.hall[h] = l
         w.rooms[i][room_loc] = nil
         rv.push([w, energy_out + ((i + 1) * 2 - h).abs * ENERGY_MAP[l]])
       end
     end
+
     done_moves = rv.select {|l| l[0].done?}
     if done_moves.size > 0
       done_moves
@@ -116,20 +127,26 @@ World = Struct.new(:rooms, :hall) do
   end
 end
 
-def world_from_data(data)
+def world_from_data(data, part_2=false)
   rv = World.new((0..3).reduce([]) {|acc| acc.push([nil, nil])}, [nil] * 11)
   lines = data.lines
-  2.times do |i|
-    row = lines[2 + i]
+  if part_2
+    lines.insert(3, '#D#C#B#A#')
+    lines.insert(4, '#D#B#A#C#')
+  end
+
+  (2...lines.size).each do |idx|
+    row = lines[idx]
     row.scan(/[A-Z]/).each_with_index do |l, j|
-      rv.rooms[j][i] = l
+      rv.rooms[j][idx - 2] = l
     end
   end
 
   rv
 end
 
-# w =
+# w = world_from_data(data)
+# pp w.token
 # l = w.rooms[0][0]
 # w.hall[0] = l
 # w.rooms[0][0] = nil
@@ -169,25 +186,18 @@ def min_energy(world, energy_used=0, cache={})
 
   min_energy = energies.keys.min
   cache[world.token] = min_energy ? (energy_used + min_energy) : -1
-  # pp "#{world.token} is #{min_energy} from #{energies[min_energy]}"
+  # pp "#{world.token} is #{min_energy} from #{energies[min_energy]} #{cache.size}"
 
-  # pp "me #{min_energy} for #{world.token}"
   min_energy
 end
 
 cache = {}
 data =File.read('day23.txt')
-puts min_energy(world_from_data(data), 0, cache)
-# puts cache
+puts min_energy(world_from_data(data, true), 0, cache)
 
-# w = World.new([['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'D']], [])
-# puts w.done?
+# puts min_energy(World.new(
+#   [%w(B D D A), %w(C C B D), [nil, nil, nil, 'C'], [nil, nil, 'C', 'A']],
+#   ['A','A', nil, nil , nil, nil, nil, 'B',nil,'B','D']
+# ))
 
-40   -12095
-4    -12055
-3000 -12051
-30
-40   - 9051
-2003 - 9011
-7000 - 7008
-8
+# puts min_energy(World.new([%w(B A A A), %w(A B B B), %w(C C C C), %w(D D D D)], [nil] * 11))
